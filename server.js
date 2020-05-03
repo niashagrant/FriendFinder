9090d8d7b57eab3d28d534f15df2c67d9326d821
Niasha Abaglo-Grant <niashagrant11@gmail.com>
2020-05-02 20:55:11 -0700

    made the Get requests to both of the html files, linked the start survey button to the suvery page, and made it so that I can view the survery page from the server

--- server.js
+++ server.js
@@ -7,11 +7,24 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("./app/public"));
+
+// just to see how to pages looked for myself
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "app/public/home.html"));
 });
+
+app.get("/survey", function (req, res) {
+  res.sendFile(path.join(__dirname, "app/public/survey.html"));
+});
+
+// var apiRoutes = require("./app/routing/apiRoutes.js");
+// // app.use(apiRoutes);
+
+// var htmlRoutes = require("./app/routing/htmlRoutes.js");
+// // app.use(htmlRoutes);
 
 app.listen(PORT, function () {
   console.log("App listening on PORT http://localhost:" + PORT);
