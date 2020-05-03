e9c4d1b53f2d8be85025bb59848e94b8d8d03a68
Niasha Abaglo-Grant <niashagrant11@gmail.com>
2020-05-02 21:00:01 -0700

    made the friends array with the blecher family

---
+++ app/data/friends.js
@@ -0,0 +1,32 @@
+var friends = [
+  {
+    name: "Bob",
+    photo:
+      "https://vignette.wikia.nocookie.net/bobs-burgers-fanon/images/5/51/Bob_with_a_burger.png/revision/latest?cb=20170920181424",
+    scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1],
+  },
+  {
+    name: "Linda",
+    photo:
+      "https://vignette.wikia.nocookie.net/bobsburgerpedia/images/b/b7/Lindab.png/revision/latest/scale-to-width-down/340?cb=20130113071354",
+    scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3],
+  },
+  {
+    name: "Tina",
+    photo:
+      "https://vignette.wikia.nocookie.net/bobsburgerpedia/images/6/6e/Tina_render.png/revision/latest/scale-to-width-down/340?cb=20130113192209",
+    scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4],
+  },
+  {
+    name: "Gene",
+    photo:
+      "https://vignette.wikia.nocookie.net/bobs-burgers-fanon/images/7/7a/Gene_belcher_render.png/revision/latest?cb=20170920184921",
+    scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5],
+  },
+  {
+    name: "Louise",
+    photo:
+      "https://vignette.wikia.nocookie.net/bobsburgerpedia/images/b/b9/Louise_render.png/revision/latest/scale-to-width-down/340?cb=20130113075349",
+    scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3],
+  },
+];
